78fee5436cf07ad5da292733c4467e5880acd1c7
LuisEnrique25 <luisrg2225@gmail.com>
2024-03-21 07:19:24 -0600

    v.101

--- src/components/WhatsPop.jsx
+++ src/components/WhatsPop.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const WhatsPop = () => {
   return (
-    <div className='fixed bottom-6 right-5 w-[40px] rounded-full bg-green-700 hover:bg-green-400 aspect-square flex items-center justify-center animate-wiggle hover:text-emerald-700 transition-colors ease-linear sm:w-[50px]'> <Link to="https://wa.me/522215870242" target='_blank' className='sm:text-2xl   font-semibold'><i className="fa-brands fa-whatsapp"></i></Link></div>
+    <div className='fixed bottom-6 right-5 w-[40px] rounded-full bg-green-500 hover:bg-green-400 aspect-square flex items-center justify-center animate-wiggle hover:text-emerald-700 transition-colors ease-linear sm:w-[50px]'> <Link to="https://wa.me/522215870242" target='_blank' className='sm:text-2xl   font-semibold'><i className="fa-brands fa-whatsapp"></i></Link></div>
   )
 }
 
